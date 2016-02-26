cd9878cb603fa85c3a22851170fe5db789f6b787
Kocet Sylvain <sylvain@monacoweb-mc.net>
2016-02-26 19:09:53 +0100

    Update Collection.php

--- module-samples/app/code/Imaginaerum/Eavmodel/Model/ResourceModel/Myeaventity/Collection.php
+++ module-samples/app/code/Imaginaerum/Eavmodel/Model/ResourceModel/Myeaventity/Collection.php
@@ -3,7 +3,10 @@
 namespace Imaginaerum\Eavmodel\Model\ResourceModel\Myeaventity;
 
 class Collection extends \Magento\Eav\Model\Entity\Collection\AbstractCollection {
-
+    /**
+     * La méthode _init du constructeur accepte 2 paramêtres qui sont le chemin vers notre modèle
+     * et le chemin vers la ressource du modèle
+     **/
     protected function _construct() {
         $this->_init('Imaginaerum\Eavmodel\Model\Myeaventity', 'Imaginaerum\Eavmodel\Model\ResourceModel\Myeaventity');
     }
